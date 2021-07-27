1db0cd7592ba17ebe6d502b8c708a2d9760637d8
skiwasaki <cask1200226@gn.iwasaki.ac.jp>
2021-07-27 10:08:59 +0900

    練習02_2

--- src/jp/ac/isc/cloud/UserSelectServlet.java
+++ src/jp/ac/isc/cloud/UserSelectServlet.java
@@ -11,10 +11,13 @@ import javax.servlet.http.*;
 /**
  * Servlet implementation class UserSelectServlet
  */
-@WebServlet("/select")
+@WebServlet("/UserSelectServlet")
 public class UserSelectServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		try {
@@ -22,24 +25,21 @@ public class UserSelectServlet extends HttpServlet {
 			Connection users = null;
 			try {
 				// MySQL用のJDBCドライバーのクラスをロードする
-				Class.forName("com.mysql.jdbc.Driver");// SQLサーバーへの接続に関するインスタンスを取得する
-				users = DriverManager.getConnection("jdbc:mysql://localhost/servlet_db", "root", "");
-
+				Class.forName("com.mysql.jdbc.Driver");
+				// SQLサーバーへの接続に関するインスタンスを取得する
+				users = DriverManager.getConnection("jdbc:mysql://localhost/servlet_db",
+						"root", "");
 				//レコードを管理する配列用意
 				ArrayList<Member> list = new ArrayList<Member>();
-
 				//SQLを実行するためのクラスを用意
 				Statement state = users.createStatement();
-
 				//SELECTした結果を入れるクラスを用意
 				ResultSet result = state.executeQuery("SELECT * FROM user_table");
-
 				//次のデータを取り出し、取り出すデータがない場合はfalseを返す
 				while (result.next()) {
 					String id = result.getString("id");
 					String name = result.getString("name");
 					String picture = result.getString("picture");
-
 					//Memberクラスに1件ずつレコードを登録
 					list.add(new Member(id, name, picture));
 				}
@@ -49,12 +49,10 @@ public class UserSelectServlet extends HttpServlet {
 				request.setAttribute("list", list);
 				RequestDispatcher rd = getServletContext().getRequestDispatcher("/WEB-INF/select.jsp");
 				rd.forward(request, response);
-
 				//クラスが存在しなかったらエラーを表示
 			} catch (ClassNotFoundException e) {
 				e.printStackTrace();
 			}
-
 			//SQL実行時エラーが発生したら、エラーを表示
 		} catch (SQLException e) {
 			e.printStackTrace();

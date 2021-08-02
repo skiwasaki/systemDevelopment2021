c1bb562621779fea4cb0a10934c9763bc9aa8917
skiwasaki <cask1200226@gn.iwasaki.ac.jp>
2021-08-02 09:37:22 +0900

    練習2_04

--- src/jp/ac/isc/cloud/DBConnection.java
+++ src/jp/ac/isc/cloud/DBConnection.java
@@ -3,15 +3,42 @@ package jp.ac.isc.cloud;
 import java.sql.*;
 
 public final class DBConnection {
-	private DBConnection() {
-	}
+	private DBConnection() {}
 
 	public static Connection openConnection() {
+		//AzureのMySQLに接続するための情報（環境変数）を取得
+		String connectString = System.getenv("Database=localdb;Data Source=127.0.0.1:52409;User Id=azure;Password=6#vWHD_$");
+		String database = ""; //データベース名を保管する変数
+		String port = ""; //データベースの場所を保管する変数
+		String username = ""; //ユーザ名を保管する変数
+		String password = ""; //パスワードを保管する変数
+		//DataBase=データベース名;port=データベースの場所;UserName=ユーザ名;PassWord=パスワード形式のデータを分解
+		String[] strArray = connectString.split(";");
+		//各変数にそれぞれの値をセットする
+		for (int i = 0; i < strArray.length; i++) {
+			String[] paramArray = strArray[i].split("=");
+			switch (i) {
+			case 0:
+				database = paramArray[1];
+				continue;
+			case 1:
+				port = paramArray[1];
+				continue;
+			case 2:
+				username = paramArray[1];
+				continue;
+			case 3:
+				password = paramArray[1];
+				continue;
+			}
+		}
+
+
 		Connection users = null;
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			users = DriverManager.getConnection(
-					"jdbc:mysql://localhost/servlet_db?useUnicode=true&characterEncoding=utf8", "root", "");
+			users = DriverManager.getConnection("jdbc:mysql://" + port + "/" + database +
+					"?useUnicode=true&characterEncoding=utf8", username, password);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
@@ -28,4 +55,5 @@ public final class DBConnection {
 			e.printStackTrace();
 		}
 	}
+
 }
